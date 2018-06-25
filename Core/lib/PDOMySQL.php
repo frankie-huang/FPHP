99b3f3669f72ad1c3226238288bedf5c587c14f2
Frankie <FrankieAFei@qq.com>
2018-06-25 16:32:26 +0800

    PDOMySQL类文件作适应性处理

--- Core/lib/PDOMySQL.php
+++ Core/lib/PDOMySQL.php
@@ -1,5 +1,6 @@
 <?php
-//header('content-type:text/html;charset=utf-8');
+namespace Core\lib;
+
 class PDOMySQL
 {
     static $__author__ = 'Frankie';
@@ -123,7 +124,7 @@ class PDOMySQL
             $dbConfig['params'][constant("PDO::ATTR_PERSISTENT")] = true;
         }
         try {
-            $this->link = new PDO($dbConfig['dsn'], $dbConfig['username'], $dbConfig['password'], $dbConfig['params']);
+            $this->link = new \PDO($dbConfig['dsn'], $dbConfig['username'], $dbConfig['password'], $dbConfig['params']);
         } catch (PDOException $e) {
             $this->throw_exception($e->getMessage());
             return false;
@@ -194,7 +195,7 @@ class PDOMySQL
     private function set_columns($dbtable)
     {
         $stmt = $this->link->query("SHOW COLUMNS FROM `" . $dbtable . "`");
-        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $res = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         foreach ($res as $array) {
             if ($array['Key'] == 'PRI') {
                 $this->columns['PRI'] = $array['Field'];
@@ -1098,12 +1099,12 @@ class PDOMySQL
             return false;
         }
         if ($is_find === true) {
-            $res = $this->PDOStatement->fetch(PDO::FETCH_ASSOC);
+            $res = $this->PDOStatement->fetch(\PDO::FETCH_ASSOC);
             if (false === $res) {
                 return null;
             }
         } else {
-            $res = $this->PDOStatement->fetchAll(PDO::FETCH_ASSOC);
+            $res = $this->PDOStatement->fetchAll(\PDO::FETCH_ASSOC);
             if (0 === count($res)) {
                 return null;
             }
@@ -1814,26 +1815,11 @@ class PDOMySQL
      */
     public function throw_exception($errMsg, $ignore_debug = false, $exit = true)
     {
-        $bt = debug_backtrace();
-        $caller = array_shift($bt);
         if ($this->dbdebug || $ignore_debug) {
-            $errMsg .= '</b><br/><br/><b>SOURCE</b><br>FILE: ' . $caller['file'] . '   LINE: ' . $caller['line'];
-            $caller = array_shift($bt);
-            $number = 0;
-            if ($caller != null) {
-                $errMsg .= '<br/><br/><b>TRACE</b><br/>';
-            }
-            while ($caller != null) {
-                $number++;
-                $errMsg .= '#' . $number . ' ' . $caller['file'] . '(' . $caller['line'] . ')<br/>';
-                $caller = array_shift($bt);
-            }
+            printError();
         } else {
-            $errMsg = "系统出错，请联系管理员。</b>";
+            printError(false);
         }
-        echo '<div style="width:80%;background-color:#ABCDEF;color:black;padding:20px 0px;"><b style="font-size:25px;">
-				' . $errMsg . '
-        </div><br/>';
         if ($exit) {
             exit(0);
         }
@@ -1892,226 +1878,3 @@ class PDOMySQL
         $this->close();
     }
 }
-
-//M函数
-function M($dbtable, $ConfigID = 0, $dbConfig = null)
-{
-    return new PDOMySQL($dbtable, $ConfigID, $dbConfig);
-}
-
-function filter(&$value)
-{
-    $value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-}
-//I函数，参数为字符串
-function I($str)
-{
-    $pos = strrpos($str, '.', -1);
-    if ($pos === false) {
-        PDOMySQL::throw_exception("I函数参数错误");
-        return false;
-    }
-    $type = substr($str, 0, $pos);
-    $param = substr($str, $pos + 1);
-    switch (strtoupper($type)) {
-        case 'GET':
-            if ($param != '') {
-                $result_set = $_GET[$param];
-            } else {
-                $result_set = $_GET;
-            }
-            break;
-        case 'POST':
-            // 如果$_POST中无数据，则从php://input中取
-            if (count($_POST) == 0) {
-                $_POST = json_decode(file_get_contents('php://input'), true);
-            }
-            if ($param != '') {
-                $result_set = $_POST[$param];
-            } else {
-                $result_set = $_POST;
-            }
-            break;
-        default:
-            PDOMySQL::throw_exception("I函数不支持此参数：" . $str);
-            return false;
-    }
-    if (is_array($result_set)) {
-        array_walk_recursive($result_set, "filter");
-    }
-    return $result_set;
-}
-
-/**
- * 获取客户端IP地址
- * @param integer $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
- * @param boolean $adv 是否进行高级模式获取（有可能被伪装）
- * @return mixed
- */
-function get_client_ip($type = 0, $adv = false)
-{
-    $type = $type ? 1 : 0;
-    static $ip = null;
-    if ($ip !== null) {
-        return $ip[$type];
-    }
-    if ($adv) {
-        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            $pos = array_search('unknown', $arr);
-            if (false !== $pos) {
-                unset($arr[$pos]);
-            }
-            $ip = trim($arr[0]);
-        } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
-    // IP地址合法验证
-    $long = sprintf("%u", ip2long($ip));
-    $ip = $long ? array($ip, $long) : array('0.0.0.0', 0);
-    return $ip[$type];
-}
-
-/**
- * Ajax方式返回数据到客户端
- * 暂时只支持返回json格式数据
- */
-function ajaxReturn($data)
-{
-    header('Content-Type:application/json; charset=utf-8');
-    $data = json_encode($data);
-    exit($data);
-}
-
-/**
- * 浏览器友好的变量输出
- * @param mixed $var 变量
- * @param boolean $echo 是否输出 默认为True 如果为false 则返回输出字符串
- * @param string $label 标签 默认为空
- * @param boolean $strict 是否严谨 默认为true
- * @return void|string
- */
-function dump($var, $echo = true, $label = null, $strict = true)
-{
-    $label = ($label === null) ? '' : rtrim($label) . ' ';
-    if (!$strict) {
-        if (ini_get('html_errors')) {
-            $output = print_r($var, true);
-            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
-        } else {
-            $output = $label . print_r($var, true);
-        }
-    } else {
-        ob_start();
-        var_dump($var);
-        $output = ob_get_clean();
-        if (!extension_loaded('xdebug')) {
-            $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
-            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
-        }
-    }
-    if ($echo) {
-        echo ($output);
-        return null;
-    } else {
-        return $output;
-    }
-}
-
-/**
- * PHP的html编码函数
- */
-function html_encode($str)
-{
-    $s = "";
-    if (strlen($str) == 0) {
-        return "";
-    }
-    $s = preg_replace('/&/', "&amp;", $str);
-    $s = preg_replace('/</', "&lt;", $s);
-    $s = preg_replace('/>/', "&gt;", $s);
-    $s = preg_replace('/ /', "&nbsp;", $s);
-    $s = preg_replace('/\'/', "&#39;", $s);
-    $s = preg_replace('/\"/', "&quot;", $s);
-    $s = preg_replace('/\n/', "<br/>", $s);
-    return $s;
-}
-
-/**
- * PHP的html解码函数
- */
-function html_decode($str)
-{
-    $s = "";
-    if (strlen($str) == 0) {
-        return "";
-    }
-    $s = preg_replace('/&lt;/', "<", $str);
-    $s = preg_replace('/&gt;/', ">", $s);
-    $s = preg_replace('/&nbsp;/', " ", $s);
-    $s = preg_replace('/&#39;/', "\'", $s);
-    $s = preg_replace('/&quot;/', "\"", $s);
-    $s = preg_replace('/&amp;/', "&", $s);
-    $s = preg_replace('/<br\/>/', "\n", $s);
-    return $s;
-}
-
-/*自己封装的dump方法，现已废弃
-function part_dump($var, $layer = 1)
-{
-$str = '';
-$blank = '&nbsp;&nbsp;';
-$_blank = '';
-if ($layer>1) {
-for ($i=1; $i<$layer; $i++) {
-$_blank .= '&nbsp;&nbsp;';
-}
-}
-$blank .= $_blank;
-if (is_array($var)) {
-$length = count($var);
-$str .= 'array('.$length.') {<br/>'.$blank;
-foreach ($var as $key => $val) {
-if (is_int($key)) {
-$str .= '['.$key.'] => '.part_dump($val, $layer+1).'<br/>'.$blank;
-} else {
-$str .= '["'.$key.'"] => '.part_dump($val, $layer+1).'<br/>'.$blank;
-}
-}
-$str = rtrim($str, '&nbsp;');
-$str .= $_blank;
-$str .= '}';
-} elseif (is_object($var)) {
-$length = count((array)$var);
-$str .= 'object('.$length.') {<br/>'.$blank;
-foreach ($var as $key => $val) {
-$str .= '["'.$key.'"] => '.part_dump($val, $layer+1).'<br/>'.$blank;
-}
-$str = rtrim($str, '&nbsp;');
-$str .= $_blank;
-$str .= '}';
-} elseif (is_string($var)) {
-$str .= 'string'.'('.strlen($var).') "'.$var.'"';
-} elseif (is_bool($var)) {
-$boolean_str = ($var===true)?'true':'false';
-$str .= 'bool('.$boolean_str.')';
-} elseif (is_null($var)) {
-$str .= 'NULL';
-} else {
-$str .= gettype($var).'('.$var.')';
-}
-return $str;
-}
-//dump函数
-function dump($var)
-{
-$str = part_dump($var);
-$str = '<pre>'.$str.'<pre>';
-print($str);
-}
- */
