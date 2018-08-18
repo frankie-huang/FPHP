e6d600ea5071f83cd451846c18d1fedac6a59920
Frankie <FrankieAFei@qq.com>
2018-08-18 15:31:14 +0800

    增加控制器定义构造函数的示例

--- App/Home/Controller/Index.php
+++ App/Home/Controller/Index.php
@@ -4,6 +4,15 @@ use Core\lib\Controller;
 
 class Index extends Controller
 {
+    // 如果子类要定义自己的构造函数，需要这样写👇
+    public function __construct($module, $controller, $action, $params = array())
+    {
+        parent::__construct($module, $controller, $action, $params);
+        /**
+         * 其他操作
+         */
+    }
+
     public function index()
     {
         $this->display();
